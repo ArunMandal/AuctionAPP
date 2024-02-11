43dab4a7c4aa1f5c5887f5749ed10b1c42637272
ArunMandal <marun1406@gmail.com>
2024-02-11 17:59:13 -0600

    Implemented Listing Index page

---
+++ AuctionAPP/Data/Services/IListingsService.cs
@@ -0,0 +1,9 @@
+﻿using AuctionAPP.Models;
+
+namespace AuctionAPP.Data.Services
+{
+    public interface IListingsService
+    {
+        IQueryable<Listing> GetAll();
+    }
+}
---
+++ AuctionAPP/Data/Services/ListingsService.cs
@@ -0,0 +1,20 @@
+﻿using AuctionAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionAPP.Data.Services
+{
+    public class ListingsService : IListingsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Listing> GetAll()
+        {
+            var applicationDbContext = _context.Listings.Include(l => l.User);
+            return applicationDbContext;
+        }
+    }
+}
